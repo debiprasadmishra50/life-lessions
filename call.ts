340d2de34307a44b59471f64983623ecba09bc29
Debi Prasad Mishra <debiprasadmishra50@gmail.com>
2022-12-26 03:25:46 +0530

    added quotes

--- call.ts
+++ call.ts
@@ -1,3 +1,29 @@
 import insertMine from "./insert";
 
-insertMine("Be Ashamed to die until you've won victory for humanity.");
+// insertMine("Be Ashamed to die until you've won victory for humanity.");
+
+// insertMine(
+//   "Tomorrow is the first-day for the rest of your life. You need to decide whether you will live or survive it."
+// );
+
+// insertMine(
+//   "You born alone and you die alone and this world just drops a bunch of rules to make you forget those facts. Never forget that."
+// );
+
+// insertMine("Make mistakes, Learn from them, Don't regret them.");
+
+// insertMine("It's always better to regret for the things that you did than you didn't.");
+
+// insertMine("Make mistakes, Learn from them, Don't regret them.");
+
+// insertMine("Don't just aspire to make a living, aspire to make a difference");
+
+// insertMine(
+//   "Just because you don’t share it on social media, doesn't mean you’re not up to big things. Live it and stay low key. Privacy is everything."
+// );
+
+// insertMine(
+//   "Without commitment, you'll never start. But more importantly, without consistency, you'll never finish."
+// );
+
+// insertMine("Do what you gotta do so you can do what you wanna do.");
